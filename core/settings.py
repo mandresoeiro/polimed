d4211f97d8a55b5aeaa8a8fc40fe7a71089d1698
mandresoeiro <mandre.soeiro@gmail.com>
2024-10-05 12:28:49 -0300

    template_base

--- core/settings.py
+++ core/settings.py
@@ -1,44 +1,54 @@
-
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-#TODO  A pasta templates e static podem ser criadas em qualquer lugar
+import sys #TODO Diz para Django onde estão nossos aplicativos
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
 
+#TODO Adicionar essa tag para que nosso projeto encontre o .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
-    }
-}
 
+#TODO Diz para Django onde estão nossos aplicativos
+APPS_DIR = str(os.path.join(BASE_DIR,'apps'))
+sys.path.insert(0, APPS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=-a@r8dj8pklnv9=hmhmq=mv*lv_e=8+hzt1@$0e+j_^oee@o3'
+#TODO SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY =  os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+#TODO DEBUG
+DEBUG = os.getenv('DEBUG')
+
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
-
-INSTALLED_APPS = [
+#TODO Aplicativos do django
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'accounts',
 ]
+THIRD_APPS = [
+    
+]
+PROJECT_APPS = [
+    'apps.base',
+    # 'apps.myapp',
+    # 'apps.pages',
+    # 'apps.student',
+]
+INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -49,8 +59,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'core.urls'
+
+
 
 TEMPLATES = [
     {
@@ -71,14 +82,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
+#TODO Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#TODO Banco de Dados.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, os.getenv('NAME_DB')),
+			#'USER':os.getenv('USER_DB')
+			#'PASSWORD': os.getenv('PASSWORD_DB')
+			#'HOST':os.getenv('HOST_DB')
+			#'PORT':os.getenv('PORT_DB')
+
+	}
 }
 
 
@@ -101,14 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+#TODO Internationalization
+# Se quiser deixar em PT BR
+LANGUAGE_CODE = 'pt-br'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
@@ -116,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-
+#TODO
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/' 
 
@@ -131,3 +149,13 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Se tiver configuração de email
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') 
+EMAIL_PORT = os.getenv('EMAIL_PORT') 
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') 
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = DEFAULT_FROM_EMAIL

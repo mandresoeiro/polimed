6fc3f670edd9a280dd2a51a854a785720b6ede07
mandresoeiro <mandre.soeiro@gmail.com>
2024-10-07 23:02:52 -0300

    Rota de cadastro

--- apps/contas/urls.py
+++ apps/contas/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from contas import views 
+from contas import views
+from django.shortcuts import render
 
 urlpatterns = [
-   path('entrar/', views.login_view, name='login'), 
+    path('timeout/',  views.timeout_view, name='timeout'),
+    path('entrar/', views.login_view, name='login'),
+    path('criar-conta/', views.register_view, name='register'),
+
+
 ]
--- apps/contas/views.py
+++ apps/contas/views.py
@@ -1,13 +1,13 @@
-from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import Group
+from contas.forms import CustomUserCreationForm
 from django.urls import path
-from contas import views
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
-# def timeout_view(request):
-#     return render(request, 'timeout.html')
+def timeout_view(request):
+    return render(request, 'timeout.html')
 
 
 def login_view(request):
@@ -23,3 +23,27 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home')
     return render(request, 'login.html')
+
+# register
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            usuario.is_valid = False
+            usuario.save()
+
+            group = Group.objects.get(name='usuario')
+            usuario.groups.add(group)
+
+            messages.success(
+                request, 'Registrado. Agora faça o login para começar!')
+            return redirect('login')
+        else:
+            # Tratar quando usuario já existe, senhas... etc...
+            messages.error(request, 'A senha deve ter pelo menos 1 caractere maiúsculo, \
+                1 caractere especial e no minimo 8 caracteres.')
+    form = CustomUserCreationForm()
+    return render(request, "register.html", {"form": form})
